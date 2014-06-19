224654c0bbe1f193016f017a6c14d9dc06ebd406
David Schlenk <david.schlenk@spanlink.com>
2014-06-19 13:16:43 -0400

    Actually fix rubocop stuff this time.

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ description      'Installs/Configures squid as a simple caching proxy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.4.3'
 
-%w({ debian ubuntu centos fedora redhat scientific suse amazon}).each do |os|
+%w{ debian ubuntu centos fedora redhat scientific suse amazon}.each do |os|
   supports os
 end
