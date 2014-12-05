9df338b6c13b7e8cd76baf3b986e4b375796f0d4
Suleyman Kutlu <skutlu@schubergphilis.com>
2014-12-05 10:13:41 +0100

    enable using variable databag names for acls, hosts and urls

--- libraries/default.rb
+++ libraries/default.rb
@@ -1,45 +1,45 @@
 # load them databags.
-def squid_load_host_acl
+def squid_load_host_acl(databag_name)
   host_acl = []
   begin
-    data_bag('squid_hosts').each do |bag|
-      group = data_bag_item('squid_hosts', bag)
+    data_bag(databag_name).each do |bag|
+      group = data_bag_item(databag_name, bag)
       group['net'].each do |host|
         host_acl.push [group['id'], group['type'], host]
       end
     end
   rescue
-    Chef::Log.info "no 'squid_hosts' data bag"
+    Chef::Log.info "no '#{databag_name}' data bag"
   end
   host_acl
 end
 
-def squid_load_url_acl
+def squid_load_url_acl(databag_name)
   url_acl = []
   begin
-    data_bag('squid_urls').each do |bag|
-      group = data_bag_item('squid_urls', bag)
+    data_bag(databag_name).each do |bag|
+      group = data_bag_item(databag_name, bag)
       group['urls'].each do |url|
         url_acl.push [group['id'], group.key?('element') ? group['element'] : node['squid']['acl_element'], url]
       end
     end
   rescue
-    Chef::Log.info "no 'squid_urls' data bag"
+    Chef::Log.info "no '#{databag_name}' data bag"
   end
   url_acl
 end
 
-def squid_load_acls
+def squid_load_acls(databag_name)
   acls = []
   begin
-    data_bag('squid_acls').each do |bag|
-      group = data_bag_item('squid_acls', bag)
+    data_bag(databag_name).each do |bag|
+      group = data_bag_item(databag_name, bag)
       group['acl'].each do |acl|
         acls.push [acl[1], group['id'], acl[0]]
       end
     end
   rescue
-    Chef::Log.info "no 'squid_acls' data bag"
+    Chef::Log.info "no '#{databag_name}' data bag"
   end
   acls
 end
