fbf485ffeb57932c11fc72505c870c7ea3acf11c
Suleyman Kutlu <skutlu@schubergphilis.com>
2014-12-24 14:44:02 +0100

    better versioning

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license          'Apache 2.0'
 description      'Installs/configures squid as a simple caching proxy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.5.3'
+version          '0.5.2001'
 
 %w{debian ubuntu centos fedora redhat scientific suse amazon}.each do |os|
   supports os
