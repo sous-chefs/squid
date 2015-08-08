bd0280eeff38b789a9630659db2aa313812799e3
Tim Smith <tim@cozy.co>
2015-08-08 15:58:07 -0700

    Minor rubocop fixes

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,7 @@
 # Cookbook Name:: squid
 # Recipe:: default
 #
-# Copyright 2013-2014, Chef Software, Inc
+# Copyright 2013-2015, Chef Software, Inc
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
