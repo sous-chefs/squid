eb456eaf14e2d0e65b2b7024b7b6fdaff8190d3b
Tim Smith <tsmith84@gmail.com>
2015-09-05 13:09:10 -0700

    Remove PercentLiteralDelimiters rule exclusion

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/configures squid as a simple caching proxy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.1'
 
-%w{debian ubuntu centos fedora redhat scientific suse amazon smartos freebsd}.each do |os|
+%w(debian ubuntu centos fedora redhat scientific suse amazon smartos freebsd).each do |os|
   supports os
 end
 
