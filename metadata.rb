fbe63558f5f507bdce13c8048356cdac174c9bfc
Tim Smith <tsmith84@gmail.com>
2015-09-05 13:19:15 -0700

    Bump metadata version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license          'Apache 2.0'
 description      'Installs/configures squid as a simple caching proxy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.1'
+version          '1.1.0'
 
 %w(debian ubuntu centos fedora redhat scientific suse amazon smartos freebsd).each do |os|
   supports os
