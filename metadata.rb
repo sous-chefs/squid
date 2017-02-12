218c42826eb21f41da623e892f0245dcf08a3621
Tim Smith <tsmith@chef.io>
2017-02-12 07:17:13 -0800

    Testing updates
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -6,14 +6,14 @@ description      'Installs/configures squid as a simple caching proxy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '3.1.0'
 
+recipe 'squid::default', 'Installs and configures Squid.'
+
 %w(debian ubuntu centos fedora redhat scientific suse amazon smartos freebsd).each do |os|
   supports os
 end
 
-recipe 'squid::default', 'Installs and configures Squid.'
+depends 'selinux_policy', '1.0.0'
 
 source_url 'https://github.com/chef-cookbooks/squid'
 issues_url 'https://github.com/chef-cookbooks/squid/issues'
-chef_version '>= 12.1'
-
-depends 'selinux_policy', '1.0.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
