f06cdb96c1a3976b2cc974a71527448b8ddc1ee3
Giovanni Toraldo <giovanni.toraldo@cloudesire.com>
2017-12-20 10:57:52 +0100

    Test no warnings are printed on startup
    Signed-off-by: Giovanni Toraldo <me@gionn.net>

--- test/integration/default/squid_spec.rb
+++ test/integration/default/squid_spec.rb
@@ -3,3 +3,13 @@ describe 'Squid server' do
     expect(port(3128)).to be_listening
   end
 end
+
+squid_syntax_check = 'sudo squid -k parse'
+if os[:family] == 'debian'
+  squid_syntax_check = 'sudo squid3 -k parse'
+end
+
+describe command(squid_syntax_check) do
+  its('exit_status') { should eq 0 }
+  its('stderr') { should_not match /WARNING/ }
+end
